e739bd5f3d410d6bfb5ceaa816c1c5631e3dbbf8
Nithid Mahattanasin <nithid.ma@ku.th>
2021-07-30 13:33:54 +0700

    Revise bug, compilable

--- openvslam_nithid/src/openvslam/tracking_module.cc
+++ openvslam_nithid/src/openvslam/tracking_module.cc
@@ -183,7 +183,7 @@ void tracking_module::track() {
         update_local_map();
 
         // pass all of the keyframes to the mapping module
-        const auto keyfrms = map_db_->get_all_keyfr9ames();
+        const auto keyfrms = map_db_->get_all_keyframes();
         for (const auto keyfrm : keyfrms) {
             mapper_->queue_keyframe(keyfrm);
         }
